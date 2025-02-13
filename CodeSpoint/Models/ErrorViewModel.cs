7428f45f4cbd01a26ced027bc6010f9cad485ef2
KISHAN KUMAR <kishaninn@gmail.com>
2025-02-13 08:07:15 +0530

    Add project files.

---
+++ CodeSpoint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeSpoint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
