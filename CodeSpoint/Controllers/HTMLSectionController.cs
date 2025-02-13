e806b6c3a1ca3acda6a5f130ca51be3c95ede1b4
KISHAN KUMAR <kishaninn@gmail.com>
2025-02-13 08:44:24 +0530

    Template, HTML-Layout ,HTML-Section-View and HTML-Section-Controller has been Added.

---
+++ CodeSpoint/Controllers/HTMLSectionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSpoint.Controllers;
+
+public class HTMLSectionController : Controller
+{
+    public IActionResult HTMLIntroduction()
+    {
+        return View();
+    }
+}
